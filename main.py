025e9ba12dac9a0168de4dd46d708bd308a98633
DaniilAnisimov <daniilanisimov2005@yandex.com>
2021-01-02 06:00:12 +0400

    Git и случайные окружности

--- main.py
+++ main.py
@@ -2,13 +2,34 @@ import sys
 from PyQt5 import uic
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
+from PyQt5 import QtCore, QtGui, QtWidgets
 import random
 
 
-class MainWindow(QMainWindow):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(500, 500)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pb_draw = QtWidgets.QPushButton(self.centralwidget)
+        self.pb_draw.setGeometry(QtCore.QRect(354, 10, 141, 23))
+        self.pb_draw.setObjectName("pb_draw")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pb_draw.setText(_translate("MainWindow", "Рисовать"))
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.setWindowTitle('Git и желтые окружности')
         self.do_paint = False
         self.initUI()
@@ -28,8 +49,8 @@ class MainWindow(QMainWindow):
             qp.end()
 
     def draw_flag(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
         for _ in range(random.randint(2, 10)):
+            qp.setBrush(QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
             r = random.randint(10, 100)
             qp.drawEllipse(random.randint(10, 490), random.randint(10, 490), r, r)
 
